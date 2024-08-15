d48614f2e9ff8b266b423480dd2b11a7e5858fdc
MD UMER THOUFIQ <110772171+THOUFIQ25@users.noreply.github.com>
2024-08-15 12:00:45 +0530

    Update AbsoDiff.java
    Accenture

--- AbsoDiff.java
+++ AbsoDiff.java
@@ -1,5 +1,5 @@
 /*int findCount(int arr[], int length, int num, int diff); 
-The function accepts an integer array ‘arr’, its length and two integer variables ‘num’ and ‘diff’. Implement this function 
+The function accepts an integer array ‘arr’, its length and two integer variables ‘num’ and ‘diff’. Implement this function
 to find and return the number of elements of ‘arr’ having an absolute difference of less than or equal to ‘diff’ with ‘num’. 
 Note: In case there is no element in ‘arr’ whose absolute difference with ‘num’ is less than or equal to ‘diff’, return -1.*/
 import java.util.*;
